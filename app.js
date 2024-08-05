96be804fc513182e163dba035f33d5cefb680d49
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-08-04 22:11:52 -0700

    add event listener to range

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ let toggleBillingInput = document.getElementById('billing-toggle');
 let rangeInput = document.getElementById('rangeSlider');
 let monthPrice = document.querySelector('.price');
 
+rangeInput.addEventListener('input', (e) => {
+    if (e.target.value === 0) {
+        return 'one';
+    }
+
+    else if (e.target.value === 1) {
+        return 'two';
+    }
+    else if (e.target.value === 2) {
+        return 'three';
+    }
+    else {
+        return 'four';
+    }
+
+});
+
 toggleBillingInput.addEventListener('change', () => {
     if (toggleBillingInput.checked) {
         twentyFiveDiscount(monthPrice);
