a20f73ef9a6bce92c66a4836d46644976865b713
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-08-08 21:40:05 -0700

    finished event listener with views and prices array

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ let toggleBillingInput = document.getElementById('billing-toggle');
 let rangeInput = document.getElementById('rangeSlider');
 let monthPrice = document.querySelector('.price');
 let pageViews = document.getElementById('viewsNumber');
-const viewsArray = [10, 50, 100, 500];
-const pricesArray = [8, 12, 16, 20];
+
+const viewsArray = [10, 50, 100, 500, 100];
+const pricesArray = [8, 12, 16, 20, 150];
 
 rangeInput.addEventListener('input', (e) => {
-    if (e.target.value === 0) {
-        return 'one';
+    let currentValue = Number(e.target.value);
+
+    if (currentValue === 0) {
+        pageViews.textContent = `${viewsArray[0]}K`; 
+        monthPrice.textContent= `$${pricesArray[0]}.00`;
     }
 
-    else if (e.target.value === 1) {
-        return 'two';
+    else if (currentValue === 1) {
+        pageViews.textContent = `${viewsArray[1]}K`; 
+        monthPrice.textContent= `$${pricesArray[1]}.00`;
     }
-    else if (e.target.value === 2) { 
-        return 'three';
+    else if (currentValue === 2) { 
+        pageViews.textContent = `${viewsArray[2]}K`; 
+        monthPrice.textContent= `$${pricesArray[2]}.00`;
     }
-    else {
-        return 'four';
+    else if (currentValue === 3) {
+        pageViews.textContent = `${viewsArray[3]}K`; 
+        monthPrice.textContent= `$${pricesArray[3]}.00`;
+    } else {
+        pageViews.textContent = `${viewsArray[4]}M`; 
+        monthPrice.textContent= `$${pricesArray[4]}.00`;
     }
 
 });
