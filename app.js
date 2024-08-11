e3da6927909d14e513f35375aed353dee34d81f5
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-08-10 20:11:14 -0700

    refactored if statement, fixed display of text contents

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let toggleBillingInput = document.getElementById('billing-toggle');
 let rangeInput = document.getElementById('rangeSlider');
-let monthPrice = document.querySelector('.price');
+let monthPrices = document.querySelectorAll('.price');
 let pageViews = document.getElementById('viewsNumber');
 
 const viewsArray = [10, 50, 100, 500, 100];
@@ -9,32 +9,17 @@ const pricesArray = [8, 12, 16, 20, 150];
 rangeInput.addEventListener('input', (e) => {
     let currentValue = Number(e.target.value);
 
-    if (currentValue === 0) {
-        pageViews.textContent = `${viewsArray[0]}K`; 
-        monthPrice.textContent= `$${pricesArray[0]}.00`;
-    }
-
-    else if (currentValue === 1) {
-        pageViews.textContent = `${viewsArray[1]}K`; 
-        monthPrice.textContent= `$${pricesArray[1]}.00`;
-    }
-    else if (currentValue === 2) { 
-        pageViews.textContent = `${viewsArray[2]}K`; 
-        monthPrice.textContent= `$${pricesArray[2]}.00`;
-    }
-    else if (currentValue === 3) {
-        pageViews.textContent = `${viewsArray[3]}K`; 
-        monthPrice.textContent= `$${pricesArray[3]}.00`;
-    } else {
-        pageViews.textContent = `${viewsArray[4]}M`; 
-        monthPrice.textContent= `$${pricesArray[4]}.00`;
-    }
-
+    pageViews.textContent = `${viewsArray[currentValue]}${currentValue === 4 ? 'M' : 'K'}`;
+    
+    monthPrices.forEach(price => {
+        price.textContent = `$${pricesArray[currentValue]}.00`;
+    });
 });
+
 
 toggleBillingInput.addEventListener('change', () => {
     if (toggleBillingInput.checked) {
-        twentyFiveDiscount(monthPrice);
+        twentyFiveDiscount(monthPrices);
     } else {
         console.log('Monthly billing selected');
         
